769be9dc6a1d1294f0cc7a563993cd337cfe4a53
Pontus Sundgren <sundgren.pontus@gmail.com>
2020-04-27 11:59:59 +0200

    Updates

---
+++ precache-manifest.f21da7b396c05d323cd587fed6ebc97b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c5afeeafe70ece3d9157061f103fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9950069b165df1b1103a",
+    "url": "/static/css/main.aefaea9f.chunk.css"
+  },
+  {
+    "revision": "ed1d245077bb9103136a",
+    "url": "/static/js/2.00b64e1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.00b64e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9950069b165df1b1103a",
+    "url": "/static/js/main.4bce9d9f.chunk.js"
+  },
+  {
+    "revision": "78595544146737490928",
+    "url": "/static/js/runtime-main.82d15cf4.js"
+  },
+  {
+    "revision": "2ac989f2f7590d7892a6474bbc37f8a6",
+    "url": "/static/media/me-irl.2ac989f2.jpeg"
+  }
+]);
