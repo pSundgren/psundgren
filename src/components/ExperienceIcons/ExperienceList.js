be674ed0f9c58b53d29e8fcfea113c6cdf669ce0
Pontus Sundgren <pontussundgren@Pontuss-MacBook-Pro.local>
2020-10-16 11:19:51 +0900

    Adds all svgs

--- src/components/ExperienceIcons/ExperienceList.js
+++ src/components/ExperienceIcons/ExperienceList.js
@@ -5,51 +5,57 @@ import ExperienceIcons from '../ExperienceIcons/ExperienceIcon'
 import ReactSVG from "../ExperienceIcons/ReactSVG";
 import ExpressSVG from "../ExperienceIcons/ExpressSVG";
 import NodeSVG from '../ExperienceIcons/NodeSVG';
-import HtmlSVG from '../ExperienceIcons/HtmlSVG'
+import HtmlSVG from '../ExperienceIcons/HtmlSVG';
+import JavaSVG from '../ExperienceIcons/JavaSVG';
+import LessSVG from '../ExperienceIcons/LessSVG';
+import AdobeCCSVG from '../ExperienceIcons/AdobeCCSVG';
+import DatabaseSVG from '../ExperienceIcons/DatabaseSVG';
+import TypeScriptSVG from '../ExperienceIcons/TypeScriptSVG';
+import JavaScriptSVG from '../ExperienceIcons/JavaScriptSVG';
+import CSharpSVG from '../ExperienceIcons/CSharpSVG';
+import CSVG from '../ExperienceIcons/CSVG';
 
-function ExperienceList(){
-    return (
-        <div className="exp-list">
-        <ExperienceIcons title="React.js">
-            <ReactSVG />
-        </ExperienceIcons>
-        <ExperienceIcons title="Express.js">
-          <ExpressSVG />
-        </ExperienceIcons>
-        <ExperienceIcons title="Node.js">
-          <NodeSVG />
-        </ExperienceIcons>
-        <ExperienceIcons title="HTML5">
-          <HtmlSVG />
-        </ExperienceIcons>
-        <ExperienceIcons title="CSS3">
-          <ExpressSVG />
-        </ExperienceIcons>
-        <ExperienceIcons title="LESS.js">
-          <ExpressSVG />
-        </ExperienceIcons>
-        <ExperienceIcons title="Adobe CC">
-          <ExpressSVG />
-        </ExperienceIcons>
-        <ExperienceIcons title="SQL & NoSQL">
-          <ExpressSVG />
-        </ExperienceIcons>
-        <ExperienceIcons title="TypeScript">
-          <ExpressSVG />
-        </ExperienceIcons>
-        <ExperienceIcons title="JavaScript">
-          <ExpressSVG />
-        </ExperienceIcons>
-        <ExperienceIcons title="Java">
-          <ExpressSVG />
-        </ExperienceIcons>
-        <ExperienceIcons title="C#">
-          <ExpressSVG />
-        </ExperienceIcons>
-        <ExperienceIcons title="C">
-          <ExpressSVG />
-        </ExperienceIcons>
-    </div>)
+function ExperienceList() {
+  return (
+    <div className="exp-list">
+      <ExperienceIcons title="React.js">
+        <ReactSVG />
+      </ExperienceIcons>
+      <ExperienceIcons title="Express.js">
+        <ExpressSVG />
+      </ExperienceIcons>
+      <ExperienceIcons title="Node.js">
+        <NodeSVG />
+      </ExperienceIcons>
+      <ExperienceIcons title="HTML5">
+        <HtmlSVG />
+      </ExperienceIcons>
+      <ExperienceIcons title="LESS.js">
+        <LessSVG />
+      </ExperienceIcons>
+      <ExperienceIcons title="Adobe CC">
+        <AdobeCCSVG />
+      </ExperienceIcons>
+      <ExperienceIcons title="SQL & NoSQL">
+        <DatabaseSVG />
+      </ExperienceIcons>
+      <ExperienceIcons title="TypeScript">
+        <TypeScriptSVG />
+      </ExperienceIcons>
+      <ExperienceIcons title="JavaScript">
+        <JavaScriptSVG />
+      </ExperienceIcons>
+      <ExperienceIcons title="Java">
+        <JavaSVG />
+      </ExperienceIcons>
+      <ExperienceIcons title="C#">
+        <CSharpSVG />
+      </ExperienceIcons>
+      <ExperienceIcons title="C">
+        <CSVG />
+      </ExperienceIcons>
+    </div>
+  )
 }
 
 export default ExperienceList;
