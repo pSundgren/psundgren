35fd254fb252760a22b5dd49caf8700f278f7939
Pontus Sundgren <pontussundgren@Pontuss-MacBook-Pro.local>
2020-10-21 13:44:40 +0900

    Fixes project images while on mobile

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -1,53 +1,79 @@
 import React from 'react';
 
-function ProjectItem(props) {
+class ProjectItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isMobile: window.innerWidth <= 770,
+            imgOrientaiton: props.imgOrientaiton,
+            exp: props.exp
+        }
+        this.updateWindowHeight = this.updateWindowHeight.bind(this);
+    }
 
-    let renderFunction = (orientaiton) => {
-        switch (orientaiton) {
-            case 'RIGHT':
-                return (
-                    <div className="proj-item">
-                        <div className="proj-text">
-                            {props.children[0]}
-                            <div className="proj-exp-list">
-                                {props.exp.map((exp) => {
-                                    return (
-                                        <p className="proj-exp" key={exp}>
-                                            {exp}
-                                        </p>
-                                    )
-                                })}
-                            </div>
-                        </div>
-                        {props.children[1]}
-                    </div>
-                );
-            default:
-                return (
-                    <div className="proj-item">
-                        {props.children[0]}
-                        <div className="proj-text">
-                            {props.children[1]}
-                            <div className="proj-exp-list">
-                                {props.exp.map((exp) => {
-                                    return (
-                                        <p className="proj-exp" key={exp}>
-                                            {exp}
-                                        </p>
-                                    )
-                                })}
-                            </div>
+    updateWindowHeight() {
+        this.setState({ isMobile: window.innerWidth <= 770 });
+    }
+
+    componentDidMount() {
+        this.updateWindowHeight();
+        window.addEventListener('resize', this.updateWindowHeight);
+    }
+
+    componentWillUnmount() {
+        window.addEventListener('resize', this.updateWindowHeight);
+    }
+
+    renderFunction = () => {
+        const { imgOrientaiton, exp } = this.props
+        const { isMobile } = this.state
+        
+        if (imgOrientaiton === 'RIGHT' && !isMobile) {
+            return (
+                <div className="proj-item">
+                    <div className="proj-text">
+                        {this.props.children[0]}
+                        <div className="proj-exp-list">
+                            {exp.map((exp) => {
+                                return (
+                                    <p className="proj-exp" key={exp}>
+                                        {exp}
+                                    </p>
+                                )
+                            })}
                         </div>
                     </div>
-                );
+                    {this.props.children[1]}
+                </div>
+            );
+        } else {
+            return (
+                <div className="proj-item">
+                    {this.props.children[1]}
+                    <div className="proj-text">
+                        {this.props.children[0]}
+                        <div className="proj-exp-list">
+                            {exp.map((exp) => {
+                                return (
+                                    <p className="proj-exp" key={exp}>
+                                        {exp}
+                                    </p>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            );
         }
     }
 
-    return (
-        <div>
-            {renderFunction(props.orientaiton)}
-        </div>
-    );
+    render() {
+        return (
+            <div>
+                {this.renderFunction()}
+            </div>
+        );
+    }
 }
 
 export default ProjectItem;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -16,13 +16,13 @@ function Projects() {
             <div className="proj-list">
                 
                 { /* SEUIL PROJECT */}
-                <ProjectItem exp={data.seuil.exp} orientaiton="LEFT">
-                    <img src={require("../../images/seuil.png")} alt="" className="proj-img left" />
+                <ProjectItem exp={data.seuil.exp} imgOrientaiton="LEFT">
                     <p className="proj-title">{data.seuil.desc}</p>
+                    <img src={require("../../images/seuil.png")} alt="" className="proj-img left" />
                 </ProjectItem>
 
                 { /* CYCL PROJECT */}
-                <ProjectItem exp={data.cycl.exp} orientaiton="RIGHT">
+                <ProjectItem exp={data.cycl.exp} imgOrientaiton="RIGHT">
                     <p className="proj-title">{data.cycl.desc}</p>
                     <img src={require("../../images/cycl.png")} alt="" className="proj-img right"/>
                 </ProjectItem>
